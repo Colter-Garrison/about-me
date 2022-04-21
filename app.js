6ebff431067205b90b5c9e6bb60db4c3a15197b3
Colter-Garrison <colter.garrison@gmail.com>
2022-04-20 19:10:33 -0700

    finished?

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-// import functions
-
 // grab DOM elements
+const aboutButton = document.getElementById('about-button');
+const aboutSection = document.getElementById('about-section');
+const aboutName = document.getElementById('name');
+
+const animalDiv = document.getElementById('animal-div');
+const animalButton = document.getElementById('animal-button');
 
 // set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
+animalButton.addEventListener('click', () => {
+    animalDiv.classList.remove('hidden');
+});
+
+aboutButton.addEventListener('click', () => {
+    if (aboutName.textContent === '____') {
+        aboutName.textContent = 'Colter';
+    } else {
+        aboutName.textContent = '____';
+    }
+
+    aboutSection.classList.toggle('my-style');
+})
